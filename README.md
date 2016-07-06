18ec7d0e24e085d0875963067a34f3d45b1f76bd
Samuel D. Leslie <sdl@nexiom.net>
2016-07-06 13:36:15 +1000

    Initial Bash scripting template

---
+++ README.md
@@ -0,0 +1,6 @@
+bash-script-template
+====================
+
+I write **Bash** scripts not infrequently and realised that I often copied a recent script whenever I started writing a new one. This provided me with a basic scaffold to work on and several useful helper functions I'd otherwise likely end up duplicating.
+
+So rather than continually copying old scripts and flensing the irrelevant code I'm publishing a more formalised template for my own usage and anyone else who may find it helpful! Suggestions for improvements are most welcome.
---
+++ template.sh
@@ -0,0 +1,188 @@
+#!/usr/bin/env bash
+
+# A best practices Bash script template with many useful functions
+
+# A better class of script...
+set -o errexit          # Exit on most errors (see the manual)
+set -o errtrace         # Make sure any error trap is inherited
+set -o nounset          # Disallow expansion of unset variables
+set -o pipefail         # Use last non-zero exit code in a pipeline
+#set -o xtrace          # Trace the execution of the script (debug)
+
+# DESC: Handler for unexpected errors
+# ARGS: $1 (optional): Exit code (defaults to 1)
+function script_trap_err() {
+    # Disable the error trap handler to prevent potential recursion
+    trap - ERR
+
+    # Consider any further errors non-fatal to ensure we run to completion
+    set +o errexit
+    set +o pipefail
+
+    # Exit with failure status
+    if [[ $# -eq 1 && $1 =~ ^[0-9]+$ ]]; then
+        exit "$1"
+    else
+        exit 1
+    fi
+}
+
+
+# DESC: Handler for exiting the script
+# ARGS: None
+function script_trap_exit() {
+    cd "$orig_cwd"
+
+    # Restore terminal colours
+    tput sgr0
+}
+
+
+# DESC: Exit script with the given message
+# ARGS: $1 (required): Message to print on exit
+#       $2 (optional): Exit code (defaults to 0)
+function script_exit() {
+    if [[ $# -eq 1 ]]; then
+        echo "$1"
+        exit 0
+    fi
+
+    if [[ $# -eq 2 && $2 =~ ^[0-9]+$ ]]; then
+        echo -e "$1"
+        # If we've been provided a non-zero exit code run the error trap
+        if [[ $2 -ne 0 ]]; then
+            script_trap_err "$2"
+        else
+            exit 0
+        fi
+    fi
+
+    script_exit "Invalid arguments passed to script_exit()!" 2
+}
+
+
+# DESC: Generic script initialisation
+# ARGS: None
+function script_init() {
+    # Useful paths
+    readonly orig_cwd="$PWD"
+    readonly script_path="${BASH_SOURCE[0]}"
+    readonly script_dir="$(dirname "$script_path")"
+    readonly script_name="$(basename "$script_path")"
+
+    # Text attributes
+    readonly ta_none="\e[0m"
+    readonly ta_bold="\e[1m"
+    readonly ta_uscore="\e[4m"
+    readonly ta_blink="\e[5m"
+    readonly ta_reverse="\e[7m"
+    readonly ta_conceal="\e[8m"
+
+    # Foreground codes
+    readonly fg_black="\e[30m"
+    readonly fg_blue="\e[34m"
+    readonly fg_cyan="\e[36m"
+    readonly fg_green="\e[32m"
+    readonly fg_magenta="\e[35m"
+    readonly fg_red="\e[31m"
+    readonly fg_white="\e[37m"
+    readonly fg_yellow="\e[33m"
+
+    # Background codes
+    readonly bg_black="\e[40m"
+    readonly bg_blue="\e[44m"
+    readonly bg_cyan="\e[46m"
+    readonly bg_green="\e[42m"
+    readonly bg_magenta="\e[45m"
+    readonly bg_red="\e[41m"
+    readonly bg_white="\e[47m"
+    readonly bg_yellow="\e[43m"
+}
+
+
+# DESC: Pretty print the provided string
+# ARGS: $1 (required): Message to print (defaults to a green foreground)
+#       $2 (optional): Colour to print the message with. This can be an ANSI
+#                      escape code or one of the prepopulated colour variables.
+function pretty_print() {
+    if [[ $# -eq 0 || $# -ge 2 ]]; then
+        script_exit "Invalid arguments passed to pretty_print()!" 2
+    fi
+
+    if [[ -z ${no_colour-} ]]; then
+        if [[ $# -eq 2 ]]; then
+            echo -n -e "$2"
+        else
+            echo -n -e "$fg_green"
+        fi
+    fi
+
+    echo "$1"
+
+    if [[ -z ${no_colour-} ]]; then
+        # Restore terminal colours
+        tput sgr0
+    fi
+}
+
+
+# DESC: Only pretty_print() the provided string if verbose mode is enabled
+# ARGS: $@ (args passed through to pretty_pretty() function)
+function verbose_print() {
+    if [[ -n ${verbose-} ]]; then
+        pretty_print "$@"
+    fi
+}
+
+
+# DESC: Usage help
+# ARGS: None
+function script_usage() {
+    echo 'Usage:
+     -h|--help                  Displays this help
+     -v|--verbose               Displays verbose output
+    -nc|--no-colour             Disables colour output'
+}
+
+
+# DESC: Parameter parser
+# ARGS: $@ (args provided to the script)
+function parse_params() {
+    local param
+    while [[ $# -gt 0 ]]; do
+        param="$1"
+        shift
+        case $param in
+            -h|--help)
+                script_usage
+                exit 0
+                ;;
+            -v|--verbose)
+                verbose="true"
+                ;;
+            -nc|--no-colour)
+                no_colour="true"
+                ;;
+            *)
+                script_exit "Invalid parameter was provided: $param" 2
+                ;;
+            esac
+    done
+}
+
+
+# DESC: Main control flow
+# ARGS: $@ (args provided to the script)
+function main() {
+    trap "script_trap_err" ERR
+    trap "script_trap_exit" EXIT
+
+    script_init
+    parse_params "$@"
+}
+
+
+# Make it rain
+main "$@"
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
