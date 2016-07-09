8d437fc5e281212e5102022f56efe5bba0601d91
Samuel D. Leslie <sdl@nexiom.net>
2016-07-09 16:22:27 +1000

    Add check_binary() function to check if a binary exists

--- template.sh
+++ template.sh
@@ -135,6 +135,28 @@ function verbose_print() {
 }
 
 
+# DESC: Check a binary exists in the search path
+# ARGS: $1 (required): Name of the binary to test for existence
+#       $2 (optional): Set non-zero to treat failure as a fatal error
+function check_binary() {
+    if [[ $# -ne 1 && $# -ne 2 ]]; then
+        script_exit "Invalid arguments passed to check_binary()!" 2
+    fi
+
+    if ! command -v "$1" > /dev/null 2>&1; then
+        if [[ ${2-} =~ ^0$ ]]; then
+            script_exit "Missing dependency: Couldn't locate $1." 1
+        else
+            verbose_print "Missing dependency: $1" "$fg_red"
+            return 1
+        fi
+    fi
+
+    verbose_print "Found dependency: $1"
+    return 0
+}
+
+
 # DESC: Usage help
 # ARGS: None
 function script_usage() {
