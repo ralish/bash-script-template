c093108adc9d93ceec15fdabbf1e1da2b64fc57a
Samuel D. Leslie <sdl@nexiom.net>
2016-07-31 11:31:51 +1000

    Mask tput commands with true as some may fail on various terminals

--- source.sh
+++ source.sh
@@ -32,7 +32,7 @@ function script_trap_exit() {
     cd "$orig_cwd"
 
     # Restore terminal colours
-    tput sgr0
+    printf '%b' "$ta_none"
 }
 
 
@@ -69,32 +69,32 @@ function script_init() {
     readonly script_name="$(basename "$script_path")"
 
     # Text attributes
-    readonly ta_none="$(tput sgr0)"
-    readonly ta_bold="$(tput bold)"
-    readonly ta_uscore="$(tput smul)"
-    readonly ta_blink="$(tput blink)"
-    readonly ta_reverse="$(tput rev)"
-    readonly ta_conceal="$(tput invis)"
+    readonly ta_none="$(tput sgr0 || true)"
+    readonly ta_bold="$(tput bold || true)"
+    readonly ta_uscore="$(tput smul || true)"
+    readonly ta_blink="$(tput blink || true)"
+    readonly ta_reverse="$(tput rev || true)"
+    readonly ta_conceal="$(tput invis || true)"
 
     # Foreground codes
-    readonly fg_black="$(tput setaf 0)"
-    readonly fg_blue="$(tput setaf 4)"
-    readonly fg_cyan="$(tput setaf 6)"
-    readonly fg_green="$(tput setaf 2)"
-    readonly fg_magenta="$(tput setaf 5)"
-    readonly fg_red="$(tput setaf 1)"
-    readonly fg_white="$(tput setaf 7)"
-    readonly fg_yellow="$(tput setaf 3)"
+    readonly fg_black="$(tput setaf 0 || true)"
+    readonly fg_blue="$(tput setaf 4 || true)"
+    readonly fg_cyan="$(tput setaf 6 || true)"
+    readonly fg_green="$(tput setaf 2 || true)"
+    readonly fg_magenta="$(tput setaf 5 || true)"
+    readonly fg_red="$(tput setaf 1 || true)"
+    readonly fg_white="$(tput setaf 7 || true)"
+    readonly fg_yellow="$(tput setaf 3 || true)"
 
     # Background codes
-    readonly bg_black="$(tput setab 0)"
-    readonly bg_blue="$(tput setab 4)"
-    readonly bg_cyan="$(tput setab 6)"
-    readonly bg_green="$(tput setab 2)"
-    readonly bg_magenta="$(tput setab 5)"
-    readonly bg_red="$(tput setab 1)"
-    readonly bg_white="$(tput setab 7)"
-    readonly bg_yellow="$(tput setab 3)"
+    readonly bg_black="$(tput setab 0 || true)"
+    readonly bg_blue="$(tput setab 4 || true)"
+    readonly bg_cyan="$(tput setab 6 || true)"
+    readonly bg_green="$(tput setab 2 || true)"
+    readonly bg_magenta="$(tput setab 5 || true)"
+    readonly bg_red="$(tput setab 1 || true)"
+    readonly bg_white="$(tput setab 7 || true)"
+    readonly bg_yellow="$(tput setab 3 || true)"
 }
 
 
