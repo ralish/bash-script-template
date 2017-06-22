a3b3057d918e26a2d18ef4c895149415ef2d2903
Samuel D. Leslie <sdl@nexiom.net>
2017-06-22 22:22:02 +1000

    Add new build_path() function for building PATH(-like) variables

--- source.sh
+++ source.sh
@@ -186,6 +186,39 @@ function verbose_print() {
 }
 
 
+# DESC: Combines two path variables and removes any duplicates
+# ARGS: $1 (required): Path(s) to join with the second argument
+#       $2 (optional): Path(s) to join with the first argument
+# OUTS: $build_path: The constructed path
+# NOTE: Heavily inspired by: https://unix.stackexchange.com/a/40973
+function build_path() {
+    if [[ -z ${1-} || $# -gt 2 ]]; then
+        script_exit "Invalid arguments passed to build_path()!" 2
+    fi
+
+    local new_path path_entry temp_path
+
+    temp_path="$1:"
+    if [[ -n ${2-} ]]; then
+        temp_path="$temp_path$2:"
+    fi
+
+    new_path=
+    while [[ -n $temp_path ]]; do
+        path_entry="${temp_path%%:*}"
+        case "$new_path:" in
+            *:"$path_entry":*) ;;
+                            *) new_path="$new_path:$path_entry"
+                               ;;
+        esac
+        temp_path="${temp_path#*:}"
+    done
+
+    # shellcheck disable=SC2034
+    build_path="${new_path#:}"
+}
+
+
 # DESC: Check a binary exists in the search path
 # ARGS: $1 (required): Name of the binary to test for existence
 #       $2 (optional): Set to any value to treat failure as a fatal error
