9283f7aa778b23a98f28dd0c1b796cc99f4019df
Samuel D. Leslie <sdl@nexiom.net>
2017-09-21 08:54:43 +1000

    Add parameter to pretty_print() to not append a new line

--- template.sh
+++ template.sh
@@ -163,13 +163,14 @@ function colour_init() {
 # ARGS: $1 (required): Message to print (defaults to a green foreground)
 #       $2 (optional): Colour to print the message with. This can be an ANSI
 #                      escape code or one of the prepopulated colour variables.
+#       $3 (optional): Set to any value to not append a new line to the message
 function pretty_print() {
-    if [[ $# -eq 0 || $# -gt 2 ]]; then
+    if [[ $# -eq 0 || $# -gt 3 ]]; then
         script_exit "Invalid arguments passed to pretty_print()!" 2
     fi
 
     if [[ -z ${no_colour-} ]]; then
-        if [[ $# -eq 2 ]]; then
+        if [[ -n ${2-} ]]; then
             printf '%b' "$2"
         else
             printf '%b' "$fg_green"
@@ -177,7 +178,11 @@ function pretty_print() {
     fi
 
     # Print message & reset text attributes
-    printf '%s%b\n' "$1" "$ta_none"
+    if [[ -n ${3-} ]]; then
+        printf '%s%b' "$1" "$ta_none"
+    else
+        printf '%s%b\n' "$1" "$ta_none"
+    fi
 }
 
 
