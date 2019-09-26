cdcaa131e6700b2939859a742fc445018ba7bbe1
Pascal Andy <pascal@firepress.org>
2019-09-26 10:57:17 -0400

    rename App_Draft

--- utility.sh
+++ utility.sh
@@ -133,6 +133,26 @@ USE CASE #3:
 
 EOF
 }
+function helpbash {
+cat << EOF
+Operator	Description
+! EXPRESSION	  The EXPRESSION is false.
+-n STRING	      The length of STRING is greater than zero.
+-z STRING	      The lengh of STRING is zero (ie it is empty).
+STRING1         = STRING2	STRING1 is equal to STRING2
+STRING1         != STRING2	STRING1 is not equal to STRING2
+INTEGER1        -eq INTEGER2	INTEGER1 is numerically equal to INTEGER2
+INTEGER1        -gt INTEGER2	INTEGER1 is numerically greater than INTEGER2
+INTEGER1        -lt INTEGER2	INTEGER1 is numerically less than INTEGER2
+    -d FILE	    FILE exists and is a directory.
+    -e FILE	    FILE exists.
+    -r FILE	    FILE exists and the read permission is granted.
+    -s FILE	    FILE exists and it's size is greater than zero (ie. it is not empty).
+    -w FILE	    FILE exists and the write permission is granted.
+    -x FILE	    FILE exists and the execute permission is granted.
+
+EOF
+}
 function master {
 # think rebase master from edge
 
@@ -150,14 +170,14 @@ function master {
     # if a tag is provided
     # it means we want to draft our CHANGELOG as well
     if [[ ! -z "${input_2}" ]] && [[ "${input_2}" != not-set ]]; then
-      draft
+      App_Draft
     fi
 
   else
     my_message="You must push your commit(s) before doing a rebase." App_Pink
   fi
 }
-function draft {
+function App_Draft {
 # think draft your release in the changelog
 # was called cl_update
 
