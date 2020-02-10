610dbf0ed345147d48a0832897c621d848a90235
Pascal Andy <pascal@firepress.org>
2020-02-09 20:18:55 -0500

    init add-on/utilities.sh
    Signed-off-by: Pascal Andy <pascal@firepress.org>

---
+++ add-on/utilities.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+function passgen_long {
+
+  grp1=$(openssl rand -base64 32 | sed 's/[^123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz]//g' | cut -c11-14) && \
+  grp2=$(openssl rand -base64 48 | sed 's/[^123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz]//g' | cut -c2-50) && \
+  grp3=$(openssl rand -base64 32 | sed 's/[^123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz]//g' | cut -c21-24) && \
+  clear && \
+  echo "${grp1}_${grp2}_${grp3}"
+}
