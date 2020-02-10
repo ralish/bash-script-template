12bbab058b1e0a4bf78633721eb9d4c9333c61a1
Pascal Andy <pascal@firepress.org>
2020-02-10 02:37:03 -0500

    Fix typo /squash

--- bashlava.sh
+++ bashlava.sh
@@ -126,7 +126,7 @@ function cl {
   if [[ "${version_before}" != "${version_after}" ]]; then
     my_message="${version_before} <== Dockerfile version before" App_Pink
     my_message="${version_after} <== Dockerfile version after" App_Pink
-    my_message="The version did NOT changed. Is it ok?" App_Pink && sleep 5
+    my_message="The versions did NOT changed. Is it ok?" App_Pink && sleep 5
   else
     my_message="${version_before} <== Dockerfile version before" App_Green
     my_message="${version_after} <== Dockerfile version after" App_Green && sleep 1
