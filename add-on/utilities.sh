e4ec3ab85db32cb02e20d91e7363b30bf0852307
Pascal Andy <pascal@firepress.org>
2020-02-10 11:53:01 -0500

    add comment
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- add-on/utilities.sh
+++ add-on/utilities.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function passgen_long {
-
+  # requires openssl
   grp1=$(openssl rand -base64 32 | sed 's/[^123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz]//g' | cut -c11-14) && \
   grp2=$(openssl rand -base64 48 | sed 's/[^123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz]//g' | cut -c2-50) && \
   grp3=$(openssl rand -base64 32 | sed 's/[^123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz]//g' | cut -c21-24) && \
