4b80939b2de7146a2b801f1bb8a9ff43fc0b59e3
Pascal Andy <pascal@firepress.org>
2020-02-09 20:18:26 -0500

    move some fct under add-on/docker
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- add-on/docker.sh
+++ add-on/docker.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-function dkr_multiline {
+function lint {
+  docker run -it --rm \
+    -v $(pwd)/Dockerfile:/Dockerfile:ro \
+    redcoolbeans/dockerlint
+}
+
+function lint_hado {
+# tk wip
+  docker run --rm hadolint/hadolint:v1.16.3-4-gc7f877d hadolint --version && echo;
+
+  docker run --rm -i hadolint/hadolint:v1.16.3-4-gc7f877d hadolint \
+    --ignore DL3000 \
+    - < Dockerfile && \
+
+  echo && \
+  docker run -v `pwd`/Dockerfile:/Dockerfile replicated/dockerfilelint /Dockerfile
+}
+
+function example_multiline {
   docker cmd on multi lines
 
   docker run -dit ... mysql $(cat <<EOF
