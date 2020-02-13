5f4605aeed85c8d0c137d5d855ad309372a971fa
Pascal Andy <pascal@firepress.org>
2020-02-13 14:22:38 -0500

    ADDED: /title-core-fct | Needed by /list
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- add-on/utilities.sh
+++ add-on/utilities.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+function title-core-fct {
+cat <<EOF > bashlava_help.md
+# Core functions are:
+EOF
+input_2="bashlava_help.md"
+App_glow && rm bashlava_help.md || true
+}
+
+# password generator. See also "passgen_long" These char are not part of the password to minimize human error: i,I,L,l,o,O,0
+function passgen { docker run ctr.run/github.com/firepress-org/alpine:master sh -c "/usr/local/bin/random3.sh";
+}
+
 function passgen_long {
   # requires openssl
   grp1=$(openssl rand -base64 32 | sed 's/[^123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz]//g' | cut -c11-14) && \
