f124c733ebf42e3b6b82e841630f19d2bc2373ec
Pascal Andy <pascal@firepress.org>
2020-02-13 17:16:09 -0500

    MOVE: cmd into utilities
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- add-on/utilities.sh
+++ add-on/utilities.sh
@@ -8,8 +8,36 @@ input_2="bashlava_help.md"
 App_glow && rm bashlava_help.md || true
 }
 
-# password generator. See also "passgen_long" These char are not part of the password to minimize human error: i,I,L,l,o,O,0
-function passgen { docker run ctr.run/github.com/firepress-org/alpine:master sh -c "/usr/local/bin/random3.sh";
+function title-utilities-fct
+ {
+cat <<EOF > bashlava_help.md
+# Utilities functions are:
+EOF
+input_2="bashlava_help.md"
+App_glow && rm bashlava_help.md || true
+}
+
+function lint {
+  docker run -it --rm \
+    -v $(pwd)/Dockerfile:/Dockerfile:ro \
+    redcoolbeans/dockerlint
+}
+
+function lint_hado {
+# tk wip
+  docker run --rm hadolint/hadolint:v1.16.3-4-gc7f877d hadolint --version && echo;
+
+  docker run --rm -i hadolint/hadolint:v1.16.3-4-gc7f877d hadolint \
+    --ignore DL3000 \
+    - < Dockerfile && \
+
+  echo && \
+  docker run -v `pwd`/Dockerfile:/Dockerfile replicated/dockerfilelint /Dockerfile
+}
+
+function passgen {
+  # password generator. See also "passgen_long" These char are not part of the password to minimize human error: i,I,L,l,o,O,0
+  docker run ctr.run/github.com/firepress-org/alpine:master sh -c "/usr/local/bin/random3.sh";
 }
 
 function passgen_long {
