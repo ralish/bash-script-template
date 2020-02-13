313e23b2ebcc7bb6d8d6bb70401041f9fcf1ec7a
Pascal Andy <pascal@firepress.org>
2020-02-13 00:53:49 -0500

    /help rename which to list
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- add-on/help.sh
+++ add-on/help.sh
@@ -15,13 +15,11 @@ cat <<EOF > bashlava_help.md
 
 ## More help:
 
-- help-bash
 - help-workflow
 
 ## See all available commands
 
-- which
-
+- list
 
 ## To test bashlava
 
@@ -74,20 +72,19 @@ function help-bash {
 cat <<EOF > bashlava_help.md
 ## Operator	Description
 
-  ! EXPRESSION	  The EXPRESSION is false.
-  -n STRING	      The length of STRING is greater than zero.
-  -z STRING	      The lengh of STRING is zero (ie it is empty).
-  STRING1         = STRING2	STRING1 is equal to STRING2
-  STRING1         != STRING2	STRING1 is not equal to STRING2
-  INTEGER1        -eq INTEGER2	INTEGER1 is numerically equal to INTEGER2
-  INTEGER1        -gt INTEGER2	INTEGER1 is numerically greater than INTEGER2
-  INTEGER1        -lt INTEGER2	INTEGER1 is numerically less than INTEGER2
-      -d FILE	    FILE exists and is a directory.
-      -e FILE	    FILE exists.
-      -r FILE	    FILE exists and the read permission is granted.
-      -s FILE	    FILE exists and its size is greater than zero (ie. it is not empty).
-      -w FILE	    FILE exists and the write permission is granted.
-      -x FILE	    FILE exists and the execute permission is granted.
+! EXPRESSION	  The EXPRESSION is false.
+-n STRING	      The length of STRING is greater than zero.
+-z STRING	      The lengh of STRING is zero (ie it is empty).
+STRING1         = STRING2	STRING1 is equal to STRING2
+STRING1         != STRING2	STRING1 is not equal to STRING2
+INTEGER1        -eq INTEGER2	INTEGER1 is numerically equal to INTEGER2
+INTEGER1        -gt INTEGER2	INTEGER1 is numerically greater than INTEGER2
+INTEGER1        -lt INTEGER2	INTEGER1 is numerically less than INTEGER2
+    -d FILE	    FILE exists and is a directory.
+    -e FILE	    FILE exists.
+    -r FILE	    FILE exists and the read permission is granted.
+    -s FILE	    FILE exists and its size is greater than zero (ie. it is not empty).
+    -w FILE	    FILE exists and the write permission is granted.
 
 EOF
 
