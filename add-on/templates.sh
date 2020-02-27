e823152cd738e12a4d0d5a2a8ef947131961cac8
Pascal Andy <pascal@firepress.org>
2020-02-27 12:42:45 -0500

    Minor update to changelog template

--- add-on/templates.sh
+++ add-on/templates.sh
@@ -2,7 +2,7 @@
 
 function init_changelog {
 cat << EOF > CHANGELOG_template.md
-We hope this **CHANGELOG** will make it easy to see what is happening with this project. Each release automatically generates/formats/updates this changeling thanks to [BashLaVa](https://github.com/firepress-org/bashlava). It uses the same « Agile Deployment Lifecycle » at FirePress. See our [blog post](https://firepress.org/en/how-do-we-update-hundreds-of-ghosts-websites-on-docker-swarm/) about it. It follows the [keep a changelog](https://keepachangelog.com/en/1.0.0/) spirit and adheres to [semantic versioning](https://semver.org/spec/v2.0.0.html).
+This **CHANGELOG** should make it easy for you to see what is happening with this project. Each release automatically generates/formats/updates this changeling thanks to [BashLaVa](https://github.com/firepress-org/bashlava). It uses the same « Agile Deployment Lifecycle » we use at FirePress. See our [blog post](https://firepress.org/en/how-do-we-update-hundreds-of-ghosts-websites-on-docker-swarm/) about it. It follows the [keep a changelog](https://keepachangelog.com/en/1.0.0/) spirit and adheres to [semantic versioning](https://semver.org/spec/v2.0.0.html).
 
 Status template:
 | ⚡️ Updates | 🚀 Added | ⚠️ Changed |
