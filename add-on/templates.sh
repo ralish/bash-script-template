4b523e5bf5f96b2e019681051672ce826aaf0a4a
Pascal Andy <pascal@firepress.org>
2020-02-27 10:41:31 -0500

    Update Dockerfile template

--- add-on/templates.sh
+++ add-on/templates.sh
@@ -30,21 +30,10 @@ ARG VERSION="notset"
 ARG RELEASE="notset"
 ARG GITHUB_USER="notset"
 
-# Default var & path. Customize if need. Usefull if you want
-# to have multiple instance of bashLaVa on your machine
-# if you rename 'bashlava.sh', rename it in App_Reset_Custom_path as well.
-ARG BASHLAVA_EXECUTABLE="bashlava.sh"
-ARG MY_LOCAL_PATH="/usr/local/bin"
-
-# Does this app accept release candidates (ie. 3.5.1-rc1) in the _version? By default = false
-# When buidling docker images it better to not have rc in the version as breaks the pattern.
-# When not working with a docker build, feel free to put this flag as true.
-# default value is false
-ARG VERSION_WITH_RELEASE_CANDIDAT="false"
-
 ###################################
 # Start you Dockerfile from here (if any)
 ###################################
+
 EOF
 my_message="File created: ${local_bashlava_path}/Dockerfile_template" App_Green
 }
